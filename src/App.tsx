de5ca32364c0a95b00242542d6f0b40127b63b31
Pavlo Hrebeniuk <pavlo@openpantry.com.au>
2023-12-06 19:07:27 +0200

    9993

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css'; // Add later
 
-const DomTreeWidget = () => {
-  const [domTree, setDomTree] = useState<any>(null);
-
-  useEffect(() => {
-    // Fetch and set the DOM tree
-    // You can use a library like dom-tree-parser to parse the DOM
-    // For simplicity, let's just use document.body for now
-    setDomTree(document.body);
-  }, []);
-
-  const handleNodeClick = (nodeId: any) => {
-    // Send a message to the parent window with the clicked node id
-    window.parent.postMessage({ type: 'highlightNode', nodeId }, '*');
-  };
-
-  const renderDomTree = (node: any) => {
-    if (!node) {
-      return null;
-    }
-
-    return (
-      <div key={node.id} className="dom-node" onClick={() => handleNodeClick(node.id)}>
-        {node.tagName}
-        {node.children && node.children.length > 0 && (
-          <div className="dom-children">
-            {node?.children?.map(renderDomTree)}
-          </div>
-        )}
-      </div>
-    );
-  };
-
+const Widget = () => {
   return (
-    <div className="dom-tree-widget">
-      <h2>DOM Tree Widget</h2>
-      {renderDomTree(domTree)}
+    <div>
+      <h2>Embedded Widget</h2>
+      {/* Your widget content goes here */}
     </div>
   );
 };
 
-export default DomTreeWidget;
+export default Widget;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App/>,
+ReactDOM.createRoot(document.getElementById('widget-root')!).render(
+  <iframe>
+    <App/>
+  </iframe>
 );
