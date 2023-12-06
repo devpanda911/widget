cd42ed19198c7f3c86467151459cab74420c7c18
Pavlo Hrebeniuk <pavlo@openpantry.com.au>
2023-12-06 16:15:28 +0200

    init 5
    (cherry picked from commit eb8230f7fa0dde016858040b3dd4f29126bc2445)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
-
-import './App.css';
 import { useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
 
 
-  const noda = window.parent.document.querySelector('body');
+  const noda = window.parent.document.querySelector('.parent');
 
-  console.log(noda);
+
   useEffect(() => {
     noda && (noda.className = 'test');
   }, [noda]);
 
 
   return (
-    <iframe>
-
-      <h1>Vite + React</h1>
+    <>
       <div>
-        {JSON.stringify(noda)}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo"/>
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => {
+          if (noda) {
+            noda.className = 'test';
+            alert(JSON.stringify(noda));
+          }
+        }}>
 
-    </iframe>
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
